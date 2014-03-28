36fad69762565119dd146c1866adcb1ce49322cc
Jan Oetjen <oetjenj@gmail.com>
2014-03-28 23:05:41 +0100

    Updated project

--- example/index.js
+++ example/index.js
@@ -2,9 +2,9 @@
  * Module dependencies
  */
 
-var _      = require('underscore');
+var _ = require('underscore');
 var helper = require('./helper');
-var cc     = require('../src');
+var cc = require('../lib');
 
 /**
  * Shortcuts
@@ -16,55 +16,45 @@ var using = helper.using;
  * Main
  */
 
- var time = function(repeat, f) {
-    var s = new Date();
-    for (var i = 0; i < repeat; ++i) f();
-    var e = new Date();
-    return e.getTime() - s.getTime();
- };
+var time = function (repeat, f) {
+  var s = new Date();
+  for (var i = 0; i < repeat; ++i) {
+    f();
+  }
+  var e = new Date();
+  return e.getTime() - s.getTime();
+};
 
-using(cc, function(p) {
-    var x;
+using(cc, function (p) {
+  var x;
 
-    x =            ' Aa Bb Cc Dd  ' +
-          '@blackBg: Aa Bb Cc Dd @defaultBg: ' +
-            '@redBg: Aa Bb Cc Dd @defaultBg: ' +
-          '@greenBg: Aa Bb Cc Dd @defaultBg: ' +
-         '@yellowBg: Aa Bb Cc Dd @defaultBg: ' +
-           '@blueBg: Aa Bb Cc Dd @defaultBg: ' +
-        '@magentaBg: Aa Bb Cc Dd @defaultBg: ' +
-           '@cyanBg: Aa Bb Cc Dd @defaultBg: ' +
-          '@whiteBg: Aa Bb Cc Dd @defaultBg: ';
+  x = ' Aa Bb Cc Dd  ' + '@blackBg: Aa Bb Cc Dd @defaultBg: ' + '@redBg: Aa Bb Cc Dd @defaultBg: ' + '@greenBg: Aa Bb Cc Dd @defaultBg: ' + '@yellowBg: Aa Bb Cc Dd @defaultBg: ' + '@blueBg: Aa Bb Cc Dd @defaultBg: ' + '@magentaBg: Aa Bb Cc Dd @defaultBg: ' + '@cyanBg: Aa Bb Cc Dd @defaultBg: ' + '@whiteBg: Aa Bb Cc Dd @defaultBg: ';
 
-    p('              defaultBg    blackBg       redBg         greenBg       yellowBg      blueBg        magentaBg     cyanBg        whiteBg');
-    p('    ' +   '@black:black    ' + x);
-    p('    ' +     '@red:red      ' + x);
-    p('    ' +   '@green:green    ' + x);
-    p('    ' +  '@yellow:yellow   ' + x);
-    p('    ' +    '@blue:blue     ' + x);
-    p('    ' + '@magenta:magenta  ' + x);
-    p('    ' +    '@cyan:cyan     ' + x);
-    p('    ' +   '@white:white    ' + x);
+  p('              defaultBg    blackBg       redBg         greenBg       yellowBg      blueBg        magentaBg     cyanBg        whiteBg');
+  p('    ' + '@black:black    ' + x);
+  p('    ' + '@red:red      ' + x);
+  p('    ' + '@green:green    ' + x);
+  p('    ' + '@yellow:yellow   ' + x);
+  p('    ' + '@blue:blue     ' + x);
+  p('    ' + '@magenta:magenta  ' + x);
+  p('    ' + '@cyan:cyan     ' + x);
+  p('    ' + '@white:white    ' + x);
 
-    console.log();
+  console.log();
 
-    x =            ' Aa Bb Cc Dd  ' +
-             '@bold: Aa Bb Cc Dd @boldOff: ' +
-            '@faint: Aa Bb Cc Dd @faintOff: ' +
-        '@underline: Aa Bb Cc Dd @underlineOff: ' +
-          '@inverse: Aa Bb Cc Dd @inverseOff: ';
+  x = ' Aa Bb Cc Dd  ' + '@bold: Aa Bb Cc Dd @boldOff: ' + '@faint: Aa Bb Cc Dd @faintOff: ' + '@underline: Aa Bb Cc Dd @underlineOff: ' + '@inverse: Aa Bb Cc Dd @inverseOff: ';
 
-    p('              default       bold          faint        underline     inverse');
-    p('    ' +   '@black:black    ' + x);
-    p('    ' +     '@red:red      ' + x);
-    p('    ' +   '@green:green    ' + x);
-    p('    ' +  '@yellow:yellow   ' + x);
-    p('    ' +    '@blue:blue     ' + x);
-    p('    ' + '@magenta:magenta  ' + x);
-    p('    ' +    '@cyan:cyan     ' + x);
-    p('    ' +   '@white:white    ' + x);
+  p('              default       bold          faint        underline     inverse');
+  p('    ' + '@black:black    ' + x);
+  p('    ' + '@red:red      ' + x);
+  p('    ' + '@green:green    ' + x);
+  p('    ' + '@yellow:yellow   ' + x);
+  p('    ' + '@blue:blue     ' + x);
+  p('    ' + '@magenta:magenta  ' + x);
+  p('    ' + '@cyan:cyan     ' + x);
+  p('    ' + '@white:white    ' + x);
 
-    console.log();
+  console.log();
 });
 
 // var i = 1000000;
